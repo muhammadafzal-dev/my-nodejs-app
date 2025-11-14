053b69e5c2b1dde3a3ca53dadfdf94a52b1c567d
afzal-obenan <muhammadafzal@obenan.com>
2025-11-14 16:09:02 +0500

    setup piple

--- scripts/clean_old_deploy.sh
+++ scripts/clean_old_deploy.sh
@@ -4,18 +4,15 @@ set -e
 echo ">>> Fixing permissions"
 sudo chown -R ubuntu:ubuntu /home/ubuntu/app-backend
 
-echo ">>> Moving to app directory"
-cd /home/ubuntu/app-backend
-
 # create backup folder
 mkdir -p /home/ubuntu/app-backend-backup
 
 # backup .env
-if [ -f .env ]; then
-  cp .env /home/ubuntu/app-backend-backup/.env
+if [ -f /home/ubuntu/app-backend/.env ]; then
+  cp /home/ubuntu/app-backend/.env /home/ubuntu/app-backend-backup/.env
 fi
 
 # backup ecosystem config
-if [ -f ecosystem.config.js ]; then
-  cp ecosystem.config.js /home/ubuntu/app-backend-backup/
+if [ -f /home/ubuntu/app-backend/ecosystem.config.js ]; then
+  cp /home/ubuntu/app-backend/ecosystem.config.js /home/ubuntu/app-backend-backup/
 fi
