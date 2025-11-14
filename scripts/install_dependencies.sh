6e876d2c1ceb9026c324f3c86189c38ffec6c787
afzal-obenan <muhammadafzal@obenan.com>
2025-11-14 11:17:49 +0500

    setup piple

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e  # exit immediately if any command fails
+
 cd /home/ubuntu/app-backend
-yarn install --production
+
+# Install all dependencies including devDependencies
+yarn install
+
+# Build the app
 yarn build
+
+yarn install --production --frozen-lockfile
