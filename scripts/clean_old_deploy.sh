8d1c921480a0634e3456a7bc1a678dce1877df7a
afzal-obenan <muhammadafzal@obenan.com>
2025-11-14 15:28:11 +0500

    setup piple

--- scripts/clean_old_deploy.sh
+++ scripts/clean_old_deploy.sh
@@ -5,17 +5,17 @@ echo ">>> Fixing permissions"
 sudo chown -R ubuntu:ubuntu /home/ubuntu/app-backend
 
 echo ">>> Moving to app directory"
-cd /home/ubuntu
+cd /home/ubuntu/app-backend
 
-# create backup folder for env and pm2 logs
-mkdir -p app-backend-backup
+# create backup folder
+mkdir -p /home/ubuntu/app-backend-backup
 
-if [ -f app-backend/.env ]; then
-  cp app-backend/.env app-backend-backup/.env
+# backup .env
+if [ -f .env ]; then
+  cp .env /home/ubuntu/app-backend-backup/.env
 fi
 
-if [ -f app-backend/ecosystem.config.js ]; then
-  cp app-backend/ecosystem.config.js app-backend-backup/
+# backup ecosystem config
+if [ -f ecosystem.config.js ]; then
+  cp ecosystem.config.js /home/ubuntu/app-backend-backup/
 fi
-
-
